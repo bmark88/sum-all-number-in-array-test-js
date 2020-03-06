df8e3b89b2ca9fb0832519c60b9203272ed65297
bmark88 <bradley.mark@hotmail.com>
2020-03-06 19:41:27 +0000

    added sumItems in an array function

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  total = 0;
+  for (let i in array) {
+    if (Array.isArray(array[i])) {
+      total += sumItems(array[i]);
+    } else {
+      total += array[i];
+    }
+  }
+
+  return total;
 }
+
+// console.log(sumItems([1, 2, 3, 4, 5]));
+// console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
+// console.log(sumItems([[[[[[[[[[[[[1]]]]]]]]]]]]]));
 
 module.exports = sumItems;
